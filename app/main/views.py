185ac7734666d886450a77eb94414780f1d8d5a7
barbaramootian <barbara.mootian@student.moringaschool.com>
2022-01-31 22:41:42 +0300

    added some changes in navbar

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,8 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template, request, redirect, url_for
 from . import main
-from ..requests import get_news,get_articles_news,get_sources_news
+from ..requests import get_news, get_articles_news, get_sources_news
 from newsapi import NewsApiClient
+
 
 @main.route('/')
 def index():
@@ -9,8 +10,9 @@ def index():
     View root page function that returns the index page and its data
     """
     all_sources = get_sources_news()
+
     title = 'Welcome to Global News'
-    return render_template('index.html',title=title,all_sources=all_sources)
+    return render_template('index.html', title=title, all_sources=all_sources, )
 
 
 @main.route('/news/<name>')
@@ -20,16 +22,17 @@ def articles(name):
     """
     View movie page function that returns the movie details page and its data
     """
+    bbc_news = get_articles_news('bbc_news')
     top_headlines = get_articles_news(name)
-    return render_template('news.html', top_headlines=top_headlines)
+    return render_template('news.html', top_headlines=top_headlines,bbc_news=bbc_news)
+
 
 @main.route('/general')
 def general():
     # # Init
     # newsapi = NewsApiClient(api_key='988fb23113204cfcb2cf79eb7ad99b76')
     """
-    View movie page function that returns the movie details page and its data
+    View news page function that returns the movie details page and its data
     """
     top_headlines = get_news('general')
     return render_template('general.html', top_headlines=top_headlines)
-
